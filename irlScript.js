97245af5a80a43da39d76b2fc70bb0b4a2169f5a
Felix Manlapig <feedaflea@gmail.com>
2025-11-22 14:46:16 -0500

    Added Canvas

--- irlScript.js
+++ irlScript.js
@@ -1,18 +1,37 @@
 import {FilesetResolver, HandLandmarker} from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision/vision_bundle.js"
-window.onload = async function() {
+
+window.onload = async () => {
+    function canvasFrame() {
+        ctx.drawImage(video, 0, 0, canvas.width, canvas.height)
+
+        const result = handLandmarker.detectForVideo(video, performance.now())
+
+        drawLandmarks(ctx, result)
+
+        requestAnimationFrame(canvasFrame)
+    }
+
     const vision = await FilesetResolver.forVisionTasks(
         "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm"
     );
-    const handLandmarker = await HandLandmarker.createFromOptions(
-        vision,
-        {
-            baseOptions: {
-                modelAssetPath: "./hand_landmarker.task"
-            },
-            numHands: 2
-        });
+    const handLandmarker = await HandLandmarker.createFromOptions(vision, {
+        baseOptions: {
+            modelAssetPath: "./hand_landmarker.task"
+        },
+        numHands: 2
+    });
 
     const video = document.getElementById("webcam")
+    const canvas = document.getElementById("outputCanvas")
+    const ctx = canvas.getContext("2d")
+
     const stream = await navigator.mediaDevices.getUserMedia({video:true})
     video.srcObject = stream
+
+    video.onloadeddata = () => {
+        canvas.width = video.videoWidth
+        canvas.height = video.videoHeight
+
+        requestAnimationFrame(canvasFrame)
+    }
 }
